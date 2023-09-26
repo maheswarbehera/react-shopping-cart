7e5c8c741c3b3769040e01c7484ecb35f1217a93
maheswar behera <bmaheswar439@gmail.com>
2023-09-26 12:24:21 +0530

    review added

--- src/component/Product/ProductDetail.js
+++ src/component/Product/ProductDetail.js
@@ -92,8 +92,8 @@ function ProductDetail() {
            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
            </svg>
-           {/* <span className="text-gray-600 ml-3">{product.rating.rate} Reviews</span> */}
-           {/* <span className="text-gray-600 ml-3">In Stock {product.rating.count}</span> */}
+            <span className="text-gray-600 ml-3">{product.rating.rate} Reviews</span> 
+            <span className="text-gray-600 ml-3">In Stock {product.rating.count}</span>
          </span>
          <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
            <Link className="text-gray-500">
