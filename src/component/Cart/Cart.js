f4bf400d27cfb8a280bdc348b2a1b5f24fdda5c8
maheswarbehera <122549684+maheswarbehera@users.noreply.github.com>
2023-09-26 06:14:00 +0000

    console log

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -22,6 +22,8 @@ const Cart = () => {
 
   const handleDec = (id)=>{
     const updateCart = carts.map(item => {
+      console.log('item id - ', item.id);
+      console.log('id - ',id);
       if(item.id === id){
         return {
           ...item ,
